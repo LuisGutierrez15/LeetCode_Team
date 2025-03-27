3a91d29dd19699584d0fa79547c7589392a9a813
Luis Angel Gutierrez Pineda <angel.gutierrez@encora.com>
2025-03-27 13:53:44 -0600

    progress in problem 3 python

---
+++ Python/Problem3.py
@@ -0,0 +1,37 @@
+import time
+
+
+class Problem3:
+    def shortestPalindrome(self, s: str) -> str:
+        if s.isspace() or s == "":
+            return s
+        # Quick thinking
+        # the length of the result string always is odd
+
+        # Step 1: mirror the word
+        s_mirror: str = s[::-1]
+        # Step 2: add a letter from original s until the total length is odd and half of s is equal to the other half
+
+        i: int = 0
+        trying: int = 1
+        while (
+            len(s_mirror) % 2 == 0
+            or s_mirror[: len(s_mirror) // 2 + 1][::-1]
+            != s_mirror[len(s_mirror) // 2 :]
+        ):
+            try:
+                s_mirror += s[i]
+                i += 1
+            except:
+                s_mirror = s[::-1]
+                i = trying
+                trying += 1
+        return s_mirror
+
+
+test: str = "abcd"
+
+p3: Problem3 = Problem3()
+print(p3.shortestPalindrome(test))
+
+s = "dcbabcd"
