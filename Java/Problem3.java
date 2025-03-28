39ac009f28e59fc95bb2714683251b221b3f083f
Luis Angel Gutierrez Pineda <angel.gutierrez@encora.com>
2025-03-28 03:35:35 -0600

    Problem 3

--- Java/Problem3.java
+++ Java/Problem3.java
@@ -14,22 +14,17 @@ public class Problem3 {
         int step = 0;
         while (true) {
             String test = s;
-            String temp = new StringBuffer(test).substring(0, step).toString();
-
+            String temp = new StringBuffer(sMirror).substring(0, step).toString();
             test = temp + test;
 
             boolean isOdd = test.length() % 2 == 1;
             int pivot = isOdd ? Math.ceilDiv(test.length(), 2) : test.length() / 2;
-            pivot -= 1;
 
             String firstPart = new StringBuffer(test).substring(0, pivot).toString();
             String firstReversed = new StringBuffer(firstPart).reverse().toString();
-            String secondPart = new StringBuffer(test).substring(pivot).toString();
+            String secondPart = new StringBuffer(test).substring(isOdd ? pivot - 1 : pivot).toString();
 
-            System.out.println(firstReversed);
-            System.out.println(secondPart);
-
-            if (firstPart.equalsIgnoreCase(secondPart)) {
+            if (firstReversed.equalsIgnoreCase(secondPart)) {
                 return test;
             }
 
@@ -38,5 +33,4 @@ public class Problem3 {
         }
 
     }
-
 }
