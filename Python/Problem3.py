d671ed8829b58d7704dace5cbeafbeae8b5e57c1
Luis Angel Gutierrez Pineda <angel.gutierrez@encora.com>
2025-03-28 00:15:06 -0600

    not passing all tests, just 111 of 126 but i have a great idea

--- Python/Problem3.py
+++ Python/Problem3.py
@@ -6,7 +6,9 @@ class Problem3:
         if s.isspace() or s == "":
             return s
         # Quick thinking
-        # the length of the result string always is odd
+        # the length of the result string is always odd
+
+        # Correction, is not always odd
 
         # Step 1: mirror the word
         s_mirror: str = s[::-1]
@@ -14,10 +16,9 @@ class Problem3:
 
         i: int = 0
         trying: int = 1
+
         while (
-            len(s_mirror) % 2 == 0
-            or s_mirror[: len(s_mirror) // 2 + 1][::-1]
-            != s_mirror[len(s_mirror) // 2 :]
+            s_mirror[: len(s_mirror) // 2 + 1][::-1] != s_mirror[len(s_mirror) // 2 :]
         ):
             try:
                 s_mirror += s[i]
@@ -26,12 +27,14 @@ class Problem3:
                 s_mirror = s[::-1]
                 i = trying
                 trying += 1
+
+        print(s_mirror[: len(s_mirror) // 2])
+        print(s_mirror[len(s_mirror) // 2 :])
         return s_mirror
 
 
-test: str = "abcd"
+test: str = "abbacd"
+expected: str = "dcabbacd"
 
 p3: Problem3 = Problem3()
 print(p3.shortestPalindrome(test))
-
-s = "dcbabcd"
